2dc07a30e214da29592b05db6d2318064f3301ae
Avinash Gaur <avi0gaur@gmail.com>
2017-01-14 08:54:51 +0530

    Changes in trending url

--- YtDown.py
+++ YtDown.py
@@ -12,27 +12,38 @@ def download_videoFrom_youtube(url, savePath):
     video = yt.get('mp4', '360p')
     video.download(savePath)
 
+
 # To Extract the video url from youtube page.
 def extract_href(url, savePath):
-   source_code = requests.get(url).text
-   Soup = BeautifulSoup(source_code,"html.parser")
-   for vurl in Soup.findAll('a', {'class': 'yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 spf-link '}):
-            href = vurl.get('href')
-            print(str(href))
-            hreff=href.split('=')
-            print(hreff[1])
-            download_videoFrom_youtube('https://youtu.be/'+hreff[1], savePath)
+    source_code = requests.get(url).text
+    Soup = BeautifulSoup(source_code,"html.parser")
+    for vurl in Soup.findAll('a', {'class': 'yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 spf-link '}):
+        href = vurl.get('href')
+        print(str(href))
+        hreff=href.split('=')
+        print(hreff[1])
+        download_videoFrom_youtube('https://youtu.be/'+hreff[1], savePath)
 
 
 
- # calling download videos
+        # calling download videos
 def start():
-     print("Hi ! this is YTrending.")
-     dtv = input("Press 1: Downloading trending videos: ")
-     asv = input("Press 2 : To download any specific videos: ")
-     if dtv == '1':
-         savePath = input('share path to save videos: ')
-         urlTy = "https://www.youtube.com/feed/trending"
-         extract_href(urlTy, savePath)
 
+    print("Hi ! this is YTrending.")
+    dtv = input("Press 1: Downloading trending videos or 2 for downloading a: ")
+
+    savePath = input('share path to save videos: ')
+    if savePath == "":
+        savePath = 'E:\YouTube Video'
+    if dtv == '1':
+        urlTy = "https://www.youtube.com/feed/trending"
+        extract_href(urlTy, savePath)
+    else:
+        endUrl = input('Video url: ')
+        urlTy = "https://youtu.be/" +endUrl
+        print(urlTy + ': ' + savePath)
+        download_videoFrom_youtube(urlTy, savePath)
+
+
+        #start code from here
 start()
